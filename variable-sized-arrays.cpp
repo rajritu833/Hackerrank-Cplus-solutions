0771def3cdb1d2b6ccb554e7fac0206057e5ccbd
Rahul Srivastava <54400720+rahulsrivastava1@users.noreply.github.com>
2021-01-20 20:10:48 +0530

    Create variable-sized-arrays.cpp

---
+++ variable-sized-arrays.cpp
@@ -0,0 +1,31 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    int x,y,s=0;
+    cin>>x>>y;
+    int *arr[x];
+    while(x--)
+    {
+        int n;
+        cin>>n;
+        arr[s]=new int[n];
+        for(int i=0;i<n;i++)
+        {
+            cin>>arr[s][i];
+        }
+        s++;
+    }  
+    while(y--)
+    {
+        int a,b;
+        cin>>a>>b;
+        cout<<arr[a][b]<<endl;
+    }
+    return 0;
+}
