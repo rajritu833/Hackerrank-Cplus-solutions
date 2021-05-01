ff61be211fe6c9de390b597e0549be26a85aa290
Rahul Srivastava <54400720+rahulsrivastava1@users.noreply.github.com>
2021-05-02 00:37:53 +0530

    Create Rectangle Area.cpp

---
+++ Rectangle Area.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+
+using namespace std;
+/*
+ * Create classes Rectangle and RectangleArea
+ */
+ class Rectangle
+ {
+   public:
+    int width;
+    int height;  
+    void display()
+    {
+        cout<<width<<" "<<height<<endl;
+    }
+ };
+class RectangleArea : public Rectangle
+{
+    public:
+    void read_input()
+    {
+        int x,y;
+        cin>>x>>y;
+        width=x;
+        height=y;
+    }
+    void display()
+    {
+        cout<<width*height<<endl;
+    }
+}; 
+
+int main()
+{
+    /*
+     * Declare a RectangleArea object
+     */
+    RectangleArea r_area;
+    
+    /*
+     * Read the width and height
+     */
+    r_area.read_input();
+    
+    /*
+     * Print the width and height
+     */
+    r_area.Rectangle::display();
+    
+    /*
+     * Print the area
+     */
+    r_area.display();
+    
+    return 0;
+}
