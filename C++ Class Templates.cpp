114637c80f77ae18e1d6ae197d3f1645cbaea657
Rahul Srivastava <54400720+rahulsrivastava1@users.noreply.github.com>
2021-05-04 11:37:51 +0530

    Create C++ Class Templates.cpp

---
+++ C++ Class Templates.cpp
@@ -0,0 +1,52 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <cassert>
+using namespace std;
+
+/*Write the class AddElements here*/
+#define cin ios_base::sync_with_stdio(false);cin.tie(NULL); cin
+#define concatenate add
+
+/*Write the class AddElements here*/
+template<typename T>
+class AddElements{
+    private:
+    T element;
+    public:
+    AddElements(T element){  
+        this->element = element;
+    }
+    T add(T a){ return element + a;}
+
+};
+
+int main () {
+  int n,i;
+  cin >> n;
+  for(i=0;i<n;i++) {
+    string type;
+    cin >> type;
+    if(type=="float") {
+        double element1,element2;
+        cin >> element1 >> element2;
+        AddElements<double> myfloat (element1);
+        cout << myfloat.add(element2) << endl;
+    }
+    else if(type == "int") {
+        int element1, element2;
+        cin >> element1 >> element2;
+        AddElements<int> myint (element1);
+        cout << myint.add(element2) << endl;
+    }
+    else if(type == "string") {
+        string element1, element2;
+        cin >> element1 >> element2;
+        AddElements<string> mystring (element1);
+        cout << mystring.concatenate(element2) << endl;
+    }
+  }
+  return 0;
+}
